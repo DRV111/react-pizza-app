7b2579fac20370d6e7832abd7d0d443ecd901fe5
DRV111 <xblackcrownx@gmail.com>
2023-10-03 12:13:49 +0300

    feat: add pizza menu conditional rendering

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,14 +2,19 @@ import Pizza from './Pizza';
 import '../index.css';
 import data from '../data/data.js';
 function Menu() {
+  const pizzaNumber = data.length;
   return (
     <main className="menu">
       <h2>Our Menu!</h2>
-      <ul className="pizzas">
-        {data.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {pizzaNumber > 0 ? (
+        <ul className="pizzas">
+          {data.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      ) : (
+        <p>Sorry, No item found!</p>
+      )}
     </main>
   );
 }
